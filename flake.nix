94c5085b13be9b448aa7b6b00853225f779b136f
mc <mmclinton@pm.me>
2024-10-15 19:20:25 -0500

    3621

--- flake.nix
+++ flake.nix
@@ -11,26 +11,29 @@
   };
 
   outputs = { self, nixpkgs, ... }@inputs: {
-    nixosConfigurations.default = nixpkgs.lib.nixosSystem {
-      specialArgs = { inherit inputs; };
-      modules = [
-        ./nixos/configuration.nix
-        ./nixos/modules/boot.nix
-        ./nixos/modules/desktop.nix
-        ./nixos/modules/fonts.nix
-        ./nixos/modules/locale.nix
-        ./nixos/modules/system-packages.nix
-        ./nixos/modules/hardware/audio.nix
-        ./nixos/modules/hardware/networking.nix
-        ./nixos/modules/programs/bash.nix
-        ./nixos/modules/programs/fish.nix
-        ./nixos/modules/programs/neovim.nix
-        ./nixos/modules/programs/programs-enable.nix
-        ./nixos/modules/services/avahi.nix
-        ./nixos/modules/services/pipewire.nix
-        ./nixos/modules/services/services-enable.nix
-        inputs.home-manager.nixosModules.default
-      ];
+    nixosConfigurations = {
+      default = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        specialArgs = { inherit inputs; };
+        modules = [
+          ./nixos/configuration.nix
+          ./nixos/modules/boot.nix
+          ./nixos/modules/desktop.nix
+          ./nixos/modules/fonts.nix
+          ./nixos/modules/locale.nix
+          ./nixos/modules/system-packages.nix
+          ./nixos/modules/hardware/audio.nix
+          ./nixos/modules/hardware/networking.nix
+          ./nixos/modules/programs/bash.nix
+          ./nixos/modules/programs/fish.nix
+          ./nixos/modules/programs/neovim.nix
+          ./nixos/modules/programs/programs-enable.nix
+          ./nixos/modules/services/avahi.nix
+          ./nixos/modules/services/pipewire.nix
+          ./nixos/modules/services/services-enable.nix
+          inputs.home-manager.nixosModules.default
+        ];
+      };
     };
   };
 }
