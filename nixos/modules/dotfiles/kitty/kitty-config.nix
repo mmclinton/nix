0fc27dc3a1925b7bd6cef0a52b48e6a951692048
mc <mmclinton@pm.me>
2025-01-04 18:50:13 -0600

    45117

--- nixos/modules/dotfiles/kitty/kitty-config.nix
+++ nixos/modules/dotfiles/kitty/kitty-config.nix
@@ -1,22 +1,28 @@
 ''
   # Window sizing when launching the application
-  remember_window_size no
-  initial_window_width 120c
+  remember_window_size  no
+  initial_window_width  120c
   initial_window_height 40c
-  enable_true_color yes
+  enable_true_color     yes
 
   # Cosmetic preferences 
-  hide_window_decorations yes
-  tab_bar_style powerline
-  window_padding_width 8
+  hide_window_decorations  yes
+  tab_bar_style            powerline
+  window_padding_width     8
   # wayland_titlebar_color system
 
   # Font
-  font_size 13.0
-  font_family      TX-02-Regular
-  bold_font        TX-02-Bold
-  italic_font      TX-02-Oblique
-  bold_italic_font TX-02-Bold-Oblique
+  font_size        13.0
+  font_family      BerkeleyMono-Regular
+  bold_font        BerkeleyMono-Bold
+  italic_font      BerkeleyMono-Oblique
+  bold_italic_font BerkeleyMono-Bold-Oblique
+
+  # Performance
+  input_delay        0
+  repaint_delay      2
+  sync_to_monitor    no
+  wayland_enable_ime no
 
   # BEGIN_KITTY_THEME
   # OneDark-Pro
